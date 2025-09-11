9abffe328dbbec823a545deec09c1868aa9b8ffb
Ігор Полинько <igor.polynko@gmail.com>
2025-09-11 14:20:37 +0300

    Added LB2
    docs - звіти, результати
    react - файли роботи

---
+++ 1-2/react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyInfo from "./components/MyInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <MyInfo />
+    </div>
+  );
+}
+
+export default App;
